e99bef852b6889dfb737f8415f3f7c6cfc25c417
ConsolattaChepchirchir <consochep97@gmail.com>
2023-03-20 19:28:47 +0300

    complete array find method assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  { year: "2012", result: "W"},
+  { year: "2011", result: "N/A"},
+  { year: "2010", result: "N/A"},
+  { year: "2009", result: "W"},
+  { year: "2008", result: "W"},
+  { year: "2007", result: "L"},
+
+]
+function superbowlWin(record)
+{
+    for(let element of record){
+        if(element.result ==="W"){
+            return element.year
+        }
+    }
+} 
+record.find(superbowlWin)
